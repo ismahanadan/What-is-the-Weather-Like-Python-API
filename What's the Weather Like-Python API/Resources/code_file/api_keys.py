db7d7a09fc65e2a99ada56c5e3ec4e0d3f97a6fb
Ismahan M. Adan <Ismahan.adan7@gmail.com>
2021-04-03 02:23:33 -0400

    Update api_keys.py

--- What's the Weather Like-Python API/Resources/code_file/api_keys.py
+++ What's the Weather Like-Python API/Resources/code_file/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "2cda8647b87c242ef324addcfc9071c0"
+weather_api_key = "API KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyAyrpOYB-_SPA3v-oOdIKzTrFo9PUc2Wd8"
+g_key = "API KEY HERE"
