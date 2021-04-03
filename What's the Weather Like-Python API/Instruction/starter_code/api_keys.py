32e57c1a332ebd308bc399c566947d1bc69df0c7
Ismahan Adan <ismahanadan@ismahans-mbp.home>
2021-04-03 02:10:47 -0400

    final version

---
+++ What's the Weather Like-Python API/Instruction/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "2cda8647b87c242ef324addcfc9071c0"
+
+# Google API Key
+g_key = "AIzaSyAyrpOYB-_SPA3v-oOdIKzTrFo9PUc2Wd8"
--- What's the Weather Like-Python API/Instructions/starter_code/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = ""
-
-# Google API Key
-g_key = ""
